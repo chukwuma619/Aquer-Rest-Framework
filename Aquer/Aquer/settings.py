ccfa7a48662bcd66aaa88d81774590e07e996ab0
Chukwuma Ebube <chukwuma619@gmail.com>
2023-06-06 16:45:26 +0100

    ALLOWED_HOSTS to  *

--- Aquer/Aquer/settings.py
+++ Aquer/Aquer/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:5500"]
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "RestApi.User"
 
