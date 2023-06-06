7ff8ec5ad5f5d0c64b6b0d52bfe1c3322b656d97
Chukwuma Ebube <chukwuma619@gmail.com>
2023-06-06 14:51:06 +0100

    modify to default db

--- Aquer/Aquer/settings.py
+++ Aquer/Aquer/settings.py
@@ -96,10 +96,16 @@ WSGI_APPLICATION = 'Aquer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(env('EXTERNAL_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(env('EXTERNAL_URL'))
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
